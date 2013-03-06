1cd42168c3ba2cce1c9bf53e188c54ec866c47cc
John de Kroon <johndekroon@gmail.com>
2013-03-06 13:26:31 +0100

    added support for loading multiple views

--- application/core/DM_UI_Controller.php
+++ application/core/DM_UI_Controller.php
@@ -4,24 +4,29 @@ class DM_UI_Controller extends DM_Base_Controller
  function __construct()
  {
   parent::__construct();
-		$this->load->view('base/header');	
  }
 
-
-	public function viewloader($views)
+	public function viewloader($views, $parms = NULL)
 	{
 		if(isset($views))
 		{
 			//If array user wants to load multiple views
 			if(is_array($views))
 			{
-				//code to load more views
+				$this->load->view('base/header');
+				foreach ($views as $key => $view) 
+				{
+					echo $key;
+					$data = $parms[$key];
+    				$this->load->view($view, $data);
+				}
+				$this->load->view('base/footer');	
 			}
 			//user wants to load a single view
 			else
 			{
 				$this->load->view('base/header');
-				$this->load->view($views);
+				$this->load->view($views, $parms);
 				$this->load->view('base/footer');	
 			}
 		}
