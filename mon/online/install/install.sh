ce3b3ad995dd965ba13e42f76d30c6a723e6cf21
John de Kroon <johndekroon@gmail.com>
2013-05-16 12:48:47 +0200

    Updated mon

--- mon/online/install/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-test="-i"
-
-if [ $1 = "-i" ]; then
-	echo "install"
-else
-	echo "not install"
-fi 
--- mon/online/online.php
+++ mon/online/online.php
@@ -1,25 +1,32 @@
+#!/usr/bin/env php
 <?php
+
 error_reporting(0);
-$url = "http://johndekroon.nl";
-$headers = get_headers($url);
 
-if(($headers[0]=="HTTP/1.1 200 OK")OR ($headers[0]=="HTTP/1.1 302 Found"))
-	post_status("ok");
-else
+if(in_array("-t", $_SERVER['argv'])&&count($_SERVER['argv'])==3)
 {
-	sleep(5);
-	$second = get_headers($url);
-	if(($second[0]=="HTTP/1.1 200 OK")OR ($second[0]=="HTTP/1.1 302 Found"))
-		post_status("warning");	
-	else
-		post_status("error");
+	$url = $_SERVER['argv'][2];
+	$headers = get_headers($url);
 	
+	if(($headers[0]=="HTTP/1.1 200 OK")OR ($headers[0]=="HTTP/1.1 302 Found"))
+		post_status("ok", "Everything is fine.");
+	else
+	{
+		sleep(10);
+		$second = get_headers($url);
+		if(($second[0]=="HTTP/1.1 200 OK")OR ($second[0]=="HTTP/1.1 302 Found"))
+			post_status("warning", "Warning: ".$url." is flapping.");	
+		else
+			post_status("error", $url." is down!");
+	}
 }
+else
+	post_status("error", "Online monitor not right configured: args are not in the right format.");
 
-function post_status($status)
+function post_status($status, $desc)
 {
 	$url = "http://daemonmaster.johndekroon.nl/post.php";
-	$postdata = http_build_query(array($status => 'some content'));
+	$postdata = http_build_query(array($status => $desc));
 	$opts = array('http' =>
 		array(
 			'method'  => 'POST',
@@ -29,6 +36,6 @@ function post_status($status)
 	);
 	$context = stream_context_create($opts);
 
-	print_r(file_get_contents($url, FALSE, $context));
+	print_r(file_get_contents($url, FALSE, $context).PHP_EOL);
 }
 
