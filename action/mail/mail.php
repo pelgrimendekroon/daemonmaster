1b4f1e962b6c7e42dd9b2d4c2b1b3e3cca958bf8
John de Kroon <johndekroon@gmail.com>
2013-05-18 23:59:15 +0200

    Made some improvements. It works now :)

--- action/mail/mail.php
+++ action/mail/mail.php
@@ -1,10 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+#!/usr/bin/php
+<?php
+if(in_array("--module", $_SERVER['argv'])&&in_array("--status", $_SERVER['argv'])&&count($_SERVER['argv'])==5)
+{
+	require_once('class.phpmailer.php');
+	$module = $_SERVER['argv'][2];
+	$status = $_SERVER['argv'][4];
+	echo "Sending your mail!! $module, $status";
+	
+	$mail             = new PHPMailer(); // defaults to using php "mail()"
+		$body             = "Hello , <br /><br /> We want to inform you that the module <i>$module</i> has changed it status to <i>$status</i>. <br /> <br /> Yours sincerely, <br /> <br />The Daemon";
+		$body             = eregi_replace("[\]",'',$body);
+		
+		$mail->AddReplyTo('', 'Do Not Reply');
+		$mail->SetFrom('', 'Daemon Master');
+		
+		$address = "";
+		$mail->AddAddress($address, "");
+		
+		$mail->Subject    = "$module changed to $status";
+		$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!";
+		$mail->MsgHTML($body);
 
-<body>
-</body>
-</html>
+		
+		if(!$mail->Send()) {
+		  echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+		  echo "Message sent!";
+		}
+
+}
+else
+{
+	echo "error";
+}
